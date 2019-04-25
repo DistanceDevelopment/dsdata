e432ca1835ee187b46a42df60a72c23f7abcc5fe
David Lawrence Miller <github@ninepointeightone.net>
2019-04-25 14:41:50 +0100

    add wren data

---
+++ R/wren.R
@@ -0,0 +1,24 @@
+#' Steve Buckland's winter wren surveys
+#'
+#' Observations of winter wren (Troglodytes troglodytes L.) collected by Steve Buckland in woodland/parkland at Montrave Estate near Leven, Fife, Scotland.
+#'
+#' Four different surveys were carried out:
+#' \describe{
+#'   \item{\code{wren1}}{5-minute point count}
+#'   \item{\code{wren2}}{snapshot method}
+#'   \item{\code{wren3}}{cue count}
+#'   \item{\code{wren4}}{line transect data}
+#' }
+#'
+#' @name wren
+#' @docType data
+#' @references Buckland, S. T. 2006. Point-transect surveys for songbirds: robust methodologies. The Auk 123 (2): 345–357.
+#' @source Steve Buckland
+#' @keywords datasets
+#' @aliases wren1 wren2 wren3 wren4 wren1_units wren2_units wren3_units wren4_units
+# @format A data frame with ...
+#   \describe{
+#     \item{column1}{description}
+#     \item{column2}{description}
+#   }
+NULL
--- inst/conversion_scripts/make_most.R
+++ inst/conversion_scripts/make_most.R
@@ -8,22 +8,33 @@ library(readdst)
 # the paths to projects
 project_paths <- c("../original/Ducknest_exercise/D702Ducknest exercise",
                    "../original/Systematic_variance_2/D702Systematic_variance_2",
+                   "../original/Wren1/D70Wren1",
+                   "../original/Wren2/D70Wren2",
                    "../original/Wren3/D70Wren3",
+                   "../original/Wren4/D70Wren4",
                    "../original/CueCountingExample/D70CueCountingExample")
 
 project_ids <- list(1,
                     "New Analysis",
                     1,
+                    1,
+                    1,
+                    1,
                     "Cue count Hazard rate No truncation")
 
 dataname <- c("ducknest",
               "Systematic_variance_2",
-              "Wren3",
+              "wren1",
+              "wren2",
+              "wren3",
+              "wren4",
               "CueCountingExample")
 
 
 
 for(i in seq_along(project_ids)){
+
+  cat("Processing", dataname[i], "\n")
 
   # convert the project
   converted <- convert_project(project_paths[i])
@@ -35,7 +46,6 @@ for(i in seq_along(project_ids)){
 
   # get the data we want and name it
   assign(paste0(dataname[i], "_units") , dat_list$units)
-#  assign(dataname[i], dat_list$data)
   assign(dataname[i], dat)
 
   save(list=c(paste0(dataname[i], "_units"), dataname[i]),

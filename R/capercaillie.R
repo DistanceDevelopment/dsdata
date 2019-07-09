e22672979ac0500fc88df11c95291c6f33b461db
David Lawrence Miller <github@ninepointeightone.net>
2019-07-09 22:12:04 +0100

    capercaillie

---
+++ R/capercaillie.R
@@ -0,0 +1,9 @@
+#' Capercaillie in Monaughty Forest
+#'
+#' Data from a line transect survey of capercaillie in Monaughty Forest, Moray, Scotland.
+#'
+#' @name capercaillie
+#' @docType data
+#' @keywords datasets
+#' @aliases capercaillie_units
+NULL
